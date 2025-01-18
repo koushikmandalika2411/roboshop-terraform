bf8147e7ee59925fd803c39952a3f09122db3cd0
Koushik Sai <mandalikakoushik@gmail.com>
2025-01-18 16:01:12 +0530

    ff

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -103,7 +103,7 @@ resource "null_resource" "ansible" {
     inline = [
       "sudo dnf install python3.12-pip -y",
       "sudo pip3.12 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/koushikmandalika2411/Roboshop-ansible roboshop.yml -e app_name=${var.component} -e ENV=${var.env}"
+      "ansible-pull -i localhost, -U https://github.com/koushikmandalika2411/Roboshop-ansible roboshop.yml -e app_name=${var.component} -e ENV=${var.env} -e vault_token=${var.vault_token}"
     ]
   }
 }
